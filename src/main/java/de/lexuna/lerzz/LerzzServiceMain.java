5194a2265c6bf1f48be655554f8a427380872aeb
lexuna <hannah.nischik@outlook.de>
2022-12-30 20:24:58 +0100

    set MongoDB

--- src/main/java/de/lexuna/lerzz/LerzzServiceMain.java
+++ src/main/java/de/lexuna/lerzz/LerzzServiceMain.java
@@ -1,9 +1,11 @@
-package de.lexuna.lerzz.server;
+package de.lexuna.lerzz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class LerzzServiceMain {
 
     public static void main(String[] args) {
