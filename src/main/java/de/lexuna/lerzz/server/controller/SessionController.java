bddc5b6c3e47858548846bb252e486329b301ac0
lexuna <hannah.nischik@outlook.de>
2023-01-03 16:30:36 +0100

    add gitignore

---
+++ src/main/java/de/lexuna/lerzz/server/controller/SessionController.java
@@ -0,0 +1,51 @@
+package de.lexuna.lerzz.server.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class SessionController {
+
+    @GetMapping("/")
+    public String process(Model model, HttpSession session) {
+        @SuppressWarnings("unchecked")
+        List<String> messages = (List<String>) session.getAttribute("MY_SESSION_MESSAGES");
+
+        if (messages == null) {
+            messages = new ArrayList<>();
+        }
+        model.addAttribute("sessionMessages", messages);
+
+        return "index";
+    }
+
+    @PostMapping("/persistMessage")
+    public String persistMessage(@RequestParam("msg") String msg, HttpServletRequest request) {
+        @SuppressWarnings("unchecked")
+        List<String> messages = (List<String>) request.getSession().getAttribute("MY_SESSION_MESSAGES");
+        if (messages == null) {
+            messages = new ArrayList<>();
+            request.getSession().setAttribute("MY_SESSION_MESSAGES", messages);
+        }
+        messages.add(msg);
+        request.getSession().setAttribute("MY_SESSION_MESSAGES", messages);
+        return "redirect:/";
+    }
+
+    @PostMapping("/destroy")
+    public String destroySession(HttpServletRequest request) {
+        request.getSession().invalidate();
+        return "redirect:/";
+    }
+
+}
